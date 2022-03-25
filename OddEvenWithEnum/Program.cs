82a48bf100054c7a30c61af4d9ea3c302330ea4d
Ricco111 <e.szugzda@gmail.com>
2022-03-25 09:45:01 +0100

    Added OddEvenWithEnum

---
+++ OddEvenWithEnum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    enum Number
+    {
+        even,
+        odd
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var x = (Number)(number % 2);
+
+            Console.WriteLine(x);
+        }
+    }
+}
